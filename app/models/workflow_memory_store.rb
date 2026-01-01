98ed1789c9633880f06792e97bdb5ecdfd58e0fb
Kyle <krmorehead@gmail.com>
2026-01-01 15:41:27 -0800

    Sisyphus: Checkpoint at 2026-01-01T23:41:27Z

--- app/models/workflow_memory_store.rb
+++ app/models/workflow_memory_store.rb
@@ -395,7 +395,7 @@ class WorkflowMemoryStore
   # @param data [Array<Hash>] Array of serialized objects
   # @param klass [Class] Class to deserialize to (must have from_h method)
   # @return [Array] Array of deserialized objects
-  def deserialize_array(data, klass)
+  def deserialize_array(data:, klass:)
     data.map { |hash| klass.from_h(**hash.deep_symbolize_keys) }
   end
 
