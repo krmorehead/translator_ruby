c9fa914bce2c64a859970cb37d42eb04b4fddf5e
Kyle <krmorehead@gmail.com>
2026-01-01 15:57:47 -0800

    Sisyphus: Checkpoint at 2026-01-01T23:57:47Z

--- test/services/workflow_state_machine_test.rb
+++ test/services/workflow_state_machine_test.rb
@@ -117,7 +117,16 @@ class WorkflowStateMachineTest < ActiveSupport::TestCase
 
     history = workflow.workflow_memory.state_history
     assert history.any?
-    assert history.all? { |h| h.is_a?(WorkflowMemories::StateTransition) }
+    
+    # Debug: check what's in the array
+    history.each_with_index do |h, i|
+      unless h.is_a?(WorkflowMemories::StateTransition)
+        puts "ERROR: history[#{i}] is #{h.class}: #{h.inspect}"
+      end
+    end
+    
+    assert history.all? { |h| h.is_a?(WorkflowMemories::StateTransition) },
+      "All history items should be StateTransition objects, got: #{history.map(&:class)}"
     # Should have at least start and finish transitions
     events = history.map(&:event)
     assert_includes events, :start
