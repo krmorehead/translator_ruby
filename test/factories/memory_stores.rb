db09d5486992f5353b653458c25615073ad22e6c
Kyle <krmorehead@gmail.com>
2026-01-01 23:56:18 -0800

    Sisyphus: Checkpoint at 2026-01-02T07:56:18Z

--- test/factories/memory_stores.rb
+++ test/factories/memory_stores.rb
@@ -52,7 +52,7 @@ FactoryBot.define do
       
       # Always create a real parent instance if not provided
       parent_instance = parent || begin
-        MemoryStore.new(path: parent_path, owner_id: owner)
+        MemoryStore.new(owner_id: owner)
       end
       
       new(
