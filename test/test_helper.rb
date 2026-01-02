552bb46b7b9a444a191089101ef17f031fb84298
Kyle <krmorehead@gmail.com>
2026-01-02 01:38:31 -0800

    Sisyphus: Checkpoint at 2026-01-02T09:38:31Z

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -38,14 +38,13 @@ module ActiveSupport
     setup do
       # Each test gets its own unique directory under the configured AGENT_DATA_PATH
       @original_agent_data_path = ENV["AGENT_DATA_PATH"]
-      @test_agent_path = File.join(@original_agent_data_path, SecureRandom.uuid)
-      ENV["AGENT_DATA_PATH"] = @test_agent_path
-      FileUtils.mkdir_p(@test_agent_path)
+      ENV["AGENT_DATA_PATH"] = File.join(@original_agent_data_path, SecureRandom.uuid)
+      FileUtils.mkdir_p(ENV["AGENT_DATA_PATH"])
     end
 
     # Automatic cleanup of test-specific AGENT_DATA_PATH after each test
     teardown do
-      FileUtils.rm_rf(@test_agent_path) if @test_agent_path && File.exist?(@test_agent_path)
+      FileUtils.rm_rf(ENV["AGENT_DATA_PATH"]) if ENV["AGENT_DATA_PATH"] && File.exist?(ENV["AGENT_DATA_PATH"])
       ENV["AGENT_DATA_PATH"] = @original_agent_data_path
     end
 
