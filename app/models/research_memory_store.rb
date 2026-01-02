2dccc8217dd95152af1bdd9bee02293caee2b64c
Kyle <krmorehead@gmail.com>
2026-01-02 01:21:57 -0800

    Sisyphus: Checkpoint at 2026-01-02T09:21:57Z

--- app/models/research_memory_store.rb
+++ app/models/research_memory_store.rb
@@ -35,7 +35,6 @@ class ResearchMemoryStore
   # @return [Array<String>] Array of owner IDs
   def self.list_owners
     base_path = ENV.fetch("AGENT_DATA_PATH")
-    return [] unless Dir.exist?(base_path)
     Dir.entries(base_path)
        .select { |entry| File.directory?(File.join(base_path, entry)) && !entry.start_with?('.') }
        .select { |entry| File.exist?(File.join(base_path, entry, "research_memory.json")) }
